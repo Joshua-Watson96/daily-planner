f0e6b5c962f01756c515d0240394f9c19e48b7a5
josh Watson <joshjkwatson@gmail.com>
2023-03-22 23:52:02 +1100

    updated documentation and README

--- Develop/script.js
+++ Develop/script.js
@@ -1,26 +1,32 @@
 // gets the DOM ready and makes the jQuery load after the browser has finished loading
 $(document).ready(function () {
-  // sets the function
+  // sets the function on click for the save button
   $(".saveBtn").on("click", function () {
     console.log(this);
-
+    //sets value of the textarea in HTML
     var textValue = $(this).siblings(".description").val();
+    //sets the value for the id attribute
     var time = $(this).parent().attr("id");
+    // adds data to the localStorage
     localStorage.setItem(time, textValue);
   });
 
+  // creates updateTimeBlock format with dayjs
   function updateTimeBlock(){
+    //gets the hour of day from dayjs
     var currentHour = dayjs().hour();
-
+    // selects time-block class
     $('.time-block').each(function(){
+      //gets the attribute id - splits out the dash between hour and x
       var timeBlockHour = parseInt($(this).attr('id').split('-')[1])
-
+      //if the hour is less than current hour; shows past colour
       if(timeBlockHour < currentHour) {
         $(this).addClass('past')
+        // If the hour is equal to the current hour; shows present colour
       } else if (timeBlockHour === currentHour) {
         $(this).removeClass('past')
         $(this).addClass('present')
-
+        // if hour is greater than the current hour; shows future colour
       } else {
         $(this).removeClass('past')
         $(this).removeClass('present')
@@ -29,10 +35,11 @@ $(document).ready(function () {
     })
 
   }
+  //runs updateTimeBlock function
   updateTimeBlock()
-
+  // shows the date in the header in date format
   $("#currentDay").text(dayjs().format('dddd, MMMM D, YYYY'))
-
+// adds user input to the local storage on save button click
   $("#hour-9 .description").val(localStorage.getItem("hour-9"));
   $("#hour-10 .description").val(localStorage.getItem("hour-10"));
   $("#hour-11 .description").val(localStorage.getItem("hour-11"));
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# daily-planner
+A daily planner: showing different colours for past(grey), present(red) and future(green) in relation to what the time is.
+
+the daily planner when you first open the deployed site
+![alt](./Assets/images/landingpage.png)
+
+the daily planner when you input text into the text area
+![alt](./Assets/images/userinput.png)
+
+the daily planner when you refresh the page after user input
+![alt](./Assets/images/refreshedpage.png)
